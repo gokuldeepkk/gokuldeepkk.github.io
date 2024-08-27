0a098722d372e427274d0a4bf11f0bbfd63b2b26
Gokul Deep <gokuldeep41@gmail.com>
2024-08-27 01:35:28 -0700

    Base Layout for New version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroTextComponent } from "./components/hero-text/hero-text.component";
+import { ProfileComponent } from './components/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroTextComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @ViewChild('circularcursor') customCursor!: ElementRef;
+  @ViewChild('main') mainWindow!: ElementRef;
   title = 'portfolio';
+
+  @HostListener('document:mousemove', ['$event'])
+  mouseMove(event: MouseEvent) {
+    this.customCursor.nativeElement.style.marginLeft = event.pageX + 'px';
+    this.customCursor.nativeElement.style.marginTop = event.pageY + 'px';
+  }
+
+  @HostListener("document:wheel", ['$event'])
+  mouseWheel(event: WheelEvent) {
+    const subdivide = 100;
+    let i = 0;
+    while(i < subdivide) {
+      this.mainWindow.nativeElement.scrollLeft += event.deltaY/subdivide;
+      i++;
+    }
+  }
 }
---
+++ src/app/components/hero-text/hero-text.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-text',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero-text.component.html',
+  styleUrl: './hero-text.component.scss'
+})
+export class HeroTextComponent {
+  // @ViewChild('heroText') heroText!: ElementRef;
+  // @HostListener('document:mousemove', ['$event'])
+  // mouseMove(event: any) {
+  //   if(event.target && event.target.className === 'effect-text') {
+  //     this.highlightText(event);
+  //   } else {
+  //     this.removeHighlight(event);
+  //   }
+  // }
+
+  // highlightText(event: MouseEvent) {
+  //   const pctX = (+event.offsetX)*100/(+this.heroText.nativeElement.offsetWidth);
+  //   const pctY = (this.getYOffset(event.target) + event.offsetY)*100/(+this.heroText.nativeElement.offsetHeight);
+  //   const bg = `radial-gradient(circle closest-corner at ${pctX}% ${pctY}%, #f15025, #ffdcd2)`;
+  //   this.heroText.nativeElement.style.backgroundImage = bg;
+  //   this.heroText.nativeElement.style.backgroundClip = 'text';
+  //   this.heroText.nativeElement.style.color = 'transparent';
+  // }
+
+  // removeHighlight(event: MouseEvent) {
+  //   this.heroText.nativeElement.style.backgroundImage = 'none';
+  //   this.heroText.nativeElement.style.backgroundClip = 'none';
+  //   this.heroText.nativeElement.style.color = '#ffffff';
+  // }
+
+  // getYOffset(element: any) {
+  //   const eleHeight = +element.offsetHeight;
+    
+  //   if(element.previousSibling && element.nextSibling) {
+  //     return eleHeight; // 2nd Text
+  //   }
+  //   if(element.previousSibling && !element.nextSibling) {
+  //     return 2 * eleHeight; //Last Text
+  //   }
+  //   return 0;
+  // }
+}
